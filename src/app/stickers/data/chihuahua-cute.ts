27d5fc62799611b8d9910d89bd58e8b72a6a548d
rafael guardeño <rafael.guardeno@in2.es>
2020-05-20 00:57:05 +0200

    before plugin

--- src/app/stickers/data/chihuahua-cute.ts
+++ src/app/stickers/data/chihuahua-cute.ts
@@ -4,15 +4,15 @@ import { StickerList } from '../stickers-list.model';
 export const chihuahuaCuteSticker: StickerList =  {
     id: 1,
     name: "Chihuahua cute",
-    imageUrl: `${environment.stickerPath}/chihuahua/chihuahua-cute-1.png`,
+    imageUrl: `${environment.stickerPath}/chihuahua-cute/icon.png`,
     stickers: [
       {
         name: "Chihuahua cute 1",
-        imageUrl: `${environment.stickerPath}/chihuahua/chihuahua-cute-1.png`
+        imageUrl: `${environment.stickerPath}/chihuahua-cute/chihuahua-cute-1.png`
       },
       {
         name: "Chihuahua cute 2",
-        imageUrl: `${environment.stickerPath}/chihuahua/chihuahua-cute-2.png`
+        imageUrl: `${environment.stickerPath}/chihuahua-cute/chihuahua-cute-2.png`
       }
     ]
   };
